602739d7a9bb11d1bcb47e00b2706b0b49eab85f
Michael Cardenas <michaelc@macs-mbp.attlocal.net>
2017-07-19 19:17:19 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "zestdemoapp" {
+  count = 3
+  ami = "ami-11716868"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-71ee470b"]
+  subnet_id = "subnet-ae0f44d8"
+  tags {
+    Name = "consul-client-${count.index}"
+  }
+
+}
